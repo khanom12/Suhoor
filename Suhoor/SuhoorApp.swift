719573b2927c93cb7d8932542cf895940255f24a
Omar Khan <omarthinksmart@gmail.com>
2026-02-16 13:34:07 -0500

    Initial Commit

---
+++ Suhoor/SuhoorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuhoorApp.swift
+//  Suhoor
+//
+//  Created by Omar Khan on 2026-02-16.
+//
+
+import SwiftUI
+
+@main
+struct SuhoorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
